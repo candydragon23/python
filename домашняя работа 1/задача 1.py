fd70fa31ada60190cdf8c446fc6a31beb63ff8c5
candydragon23 <gans2010@list.ru>
2025-12-28 23:55:37 +0800

    Delete домашняя работа 1 directory

--- домашняя работа 1/задача 1.py
+++
@@ -1,2 +0,0 @@
-l = [int(i) for i in input().split()]
-print(*[l[i-1] + l[(i+1) % len(l)] for i in range(len(l))]) if len(l) > 1 else print(*l)
--- домашняя работа 1/задача 2.py
+++
@@ -1,2 +0,0 @@
-l = [int(i) for i in input().split()]
-print(max(l) - min(l))
