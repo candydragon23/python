013bb1aa074fd92c1914ebeb7a1c0a7fc5da21f7
candydragon23 <gans2010@list.ru>
2025-12-28 23:56:15 +0800

    Delete домашняя работа 4 directory

--- домашняя работа 4/задача 1.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-dict1 = {}
-for _ in range (n):
-    arr = input().split()
-    for _ in range(1, len(arr)): dict1.update({arr[_] : arr[0]})
-m = int(input())
-arrout = input().split()
-for _ in range (m):
-    print(dict1[arrout[_]])
